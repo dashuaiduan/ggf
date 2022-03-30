573cab9c1d580217b59ea1b331f2a70a29c5793e
段大帅 <755431761@qq.com>
2022-03-30 23:37:19 +0800

    11

--- test/1_test.go
+++ test/1_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 type b struct {
@@ -16,4 +17,35 @@ func Test1(t *testing.T) {
 	a1 := a{&b{3}}
 	bb, ok := a1.bb.(*b)
 	fmt.Println(bb, ok)
+}
+
+var set = make(map[int]bool, 0)
+
+func printOnce(num int) {
+	if _, exist := set[num]; !exist {
+		fmt.Println(num)
+	}
+	set[num] = true
+}
+func Test2(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		go printOnce(100)
+	}
+	time.Sleep(time.Second)
+}
+
+type aa struct {
+	data []int
+}
+
+func getA() aa {
+	return aa{
+		data: []int{1, 2, 3},
+	}
+}
+
+func Test3(t *testing.T) {
+	a := getA()
+	a.data[0] = 66
+	fmt.Println(a)
 }
